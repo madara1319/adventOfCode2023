4dba2dab621399ec95f0994b0caa90a7b83f7669
madara1319 <mateusz.bik@op.pl>
2023-12-02 13:50:20 +0100

    Achieved second gold start for day1 YAyy

--- day1/main.js
+++ day1/main.js
@@ -1,4 +1,5 @@
 //cool option to read from file
+//___________________________________________
 function previewFile(){
   const content=document.querySelector(".content");
   const [file]=document.querySelector("input[type=file]").files;
@@ -16,6 +17,8 @@ function previewFile(){
   }
 }
 
+//___________________________________________
+//this is for js readfile method from file system
 const fs=require('fs');
 
 function readInputData(fileName){
@@ -29,6 +32,7 @@ function readInputData(fileName){
 }
 }
 
+//___________________________________________
 function extractFirstAndLastDigit(test)
 {
   if(test.length>1)
@@ -44,6 +48,7 @@ function extractFirstAndLastDigit(test)
 }
 
 
+//___________________________________________
 function sumArray(array)
 {
   let sumOfArray=0;
@@ -54,6 +59,7 @@ function sumArray(array)
   return sumOfArray;
 }
 
+//___________________________________________
 const wordsHashMap={
   "one":1,
   "two":2,
@@ -66,19 +72,29 @@ const wordsHashMap={
   "nine":9,
 }
 
-wordsArray=["one","two","three","four","five","six","seven","eight","nine"];
 
-
+//___________________________________________
 function replaceStringNumbersWithNumbers(string)
 {
   const wordToSearch=/one|two|three|four|five|six|seven|eight|nine/gi
-  let result=string.replace(wordToSearch,(match)=>{
+  let result=extractOverlappedWords(string).replace(wordToSearch,(match)=>{
     return wordsHashMap[match];
   });
   return result;
 }
+//___________________________________________
+//need this function for border situations like oneight or threeight
+function extractOverlappedWords(string)
+{
+  let resultNumber;
+  const wordToSearchLookAhead=/(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))/gi;
+  const wordToSearchLookBehind=/(?<=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))/gi;
+  let test=Array.from((string.matchAll(wordToSearchLookAhead)),(match)=>match[1]);
+    resultNumber=test.join('');
+  return resultNumber;
+}
 
-
+//this is the mainFunction that uses subfunctions to produce final result
 
 function extractNumbers(hiddenNumbers)
 {
@@ -110,9 +126,10 @@ function extractWordNumbers(hiddenNumbers)
 }
 
 
-
-//console.log(extractWordNumbers((readInputData('input2.txt'))));
-console.log(extractNumbers(extractWordNumbers((readInputData('input3.txt')))));
+//console.log(extractOverlappedWords('2eightwo5three'));
+//console.log(replaceStringNumbersWithNumbers(extractOverlappedWords('oneight')));
+//console.table(extractWordNumbers((readInputData('input3.txt'))));
+console.log(extractNumbers(extractWordNumbers((readInputData('input.txt')))));
 //extractNumbers((readInputData('input.txt')));
 //console.log(extractNumbers((readInputData('input.txt'))));
 
