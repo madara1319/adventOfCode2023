32713788d0f10e76688b7a74a16d6484521a7e8b
madara1319 <mateusz.bik@op.pl>
2023-12-06 20:32:58 +0100

    Thought the regex was correct but seems like not really

--- day2/main.js
+++ day2/main.js
@@ -17,25 +17,37 @@ function main(input)
 {
 
 const regex=/^(\b(red \b(1[3-9]|[2-9][0-9])\b)\b)|(\b(blue \b(1[5-9]|[2-9][0-9])\b)\b)|(\b(green \b(1[4-9]|[2-9][0-9])\b)\b)/;
-let arrayOfMaps=[];
-
-//you need to sum up the ids not the no of elements you moron  
+const numbersRegex=/\d+/;
+  let arrayOfMaps=[];
+  //console.log(regex.test("red 18"))
+  //console.log(regex.test("blue 1 green 2 red 18"));
+//console.log(regex.test("Game 1: 2 green 6 blue 7 red 12 green 6 blue 3 red 5 red 18 green 4 blue"))
 let counter=0;
+let invCounter=0;
  for(const element of input)
   {
+    
     let newElement=element.replace(/,|;/g,"");
+    let testElement=newElement.split(":");
+    let matches=testElement[0].match(numbersRegex);
     console.log(newElement);
-    console.log(regex.test(newElement));
-    if(regex.test(newElement)){
-     counter++; 
+    console.log(testElement[0] + " i jej matches " + matches[0]);
+    //console.log(regex.test(newElement));
+    if(!regex.test(newElement)){
+     counter=counter+1*matches[0];
     }
-    console.log(counter); 
+    else(regex.test(newElement))
+    {
+      invCounter+=1*matches[0];
+    }
+    console.log(counter);
   }
+
+    console.log("counter equals = " + counter);
+    console.log("invCounter equals = " + invCounter);
  }
 
 main(readInputData('input.txt'));
-//const regex=/^(?=.*\b(red \b([1-9]|1[0-2])\b)\b)(?=.*\b(blue \b([1-9]|1[0-2])\b)\b)(?=.*\b(green \b([1-9]|1[0-2])\b)\b).*$/;
-//console.log(regex.test("Game 2: 3 blue 45 red 26 red 26 green 56 blue 2 green 2"));
 
 
 
