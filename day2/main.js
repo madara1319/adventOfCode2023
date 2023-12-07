6b3a00cd46a666af05aa747c5827ef3f7e39405f
madara1319 <mateusz.bik@op.pl>
2023-12-07 21:18:00 +0100

    Fixed regex and collected gold star main problem in regex was incorrect order of number and word

--- day2/main.js
+++ day2/main.js
@@ -13,41 +13,31 @@ function readInputData(fileName){
 }
 }
 
-function main(input)
+function sumCorrectGamesIds(input)
 {
 
-const regex=/^(\b(red \b(1[3-9]|[2-9][0-9])\b)\b)|(\b(blue \b(1[5-9]|[2-9][0-9])\b)\b)|(\b(green \b(1[4-9]|[2-9][0-9])\b)\b)/;
+const invalidCubesAmount=/(\b(\b(1[3-9]|[2-9][0-9])\b red)\b)|(\b(\b(1[5-9]|[2-9][0-9])\b blue)\b)|(\b(\b(1[4-9]|[2-9][0-9])\b green)\b)/;
 const numbersRegex=/\d+/;
-  let arrayOfMaps=[];
-  //console.log(regex.test("red 18"))
-  //console.log(regex.test("blue 1 green 2 red 18"));
-//console.log(regex.test("Game 1: 2 green 6 blue 7 red 12 green 6 blue 3 red 5 red 18 green 4 blue"))
-let counter=0;
-let invCounter=0;
+let sumOfCorrectGamesIds=0;
  for(const element of input)
   {
     
-    let newElement=element.replace(/,|;/g,"");
-    let testElement=newElement.split(":");
-    let matches=testElement[0].match(numbersRegex);
-    console.log(newElement);
-    console.log(testElement[0] + " i jej matches " + matches[0]);
-    //console.log(regex.test(newElement));
-    if(!regex.test(newElement)){
-     counter=counter+1*matches[0];
+    let singleGame=element.replace(/,|;/g,"");
+    let singleGameArray=singleGame.split(":");
+    let gamesIDs=singleGameArray[0].match(numbersRegex);
+    //console.log(singleGame);
+    if(!invalidCubesAmount.test(singleGame)){
+     sumOfCorrectGamesIds=sumOfCorrectGamesIds+1*gamesIDs[0];
+      //console.log(regex.test(singleGame) + " czyli gry mozliwe");
     }
-    else(regex.test(newElement))
-    {
-      invCounter+=1*matches[0];
-    }
-    console.log(counter);
+    //console.log("Suma ids gierek wynosi = " + counter);
   }
-
-    console.log("counter equals = " + counter);
-    console.log("invCounter equals = " + invCounter);
+    console.log(sumOfCorrectGamesIds);
+    //console.log("counter equals = " + counter);
+    //console.log("invCounter equals = " + invCounter);
  }
 
-main(readInputData('input.txt'));
+sumCorrectGamesIds(readInputData('input.txt'));
 
 
 
